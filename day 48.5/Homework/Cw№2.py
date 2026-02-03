d19f5d5e51fd263523cab890eef439f4c909815b
GiorgiGhvdze <ghvinadzegiorgi0@gmail.com>
2026-02-03 17:48:22 +0400

    6 and halfth commit

---
+++ day 48.5/Homework/Cw№2.py
@@ -0,0 +1,7 @@
+def Even_or_Odd(num):
+    if num % 2 == 0:
+        return "Even"
+    else:
+        return "Odd"
+    
+print(Even_or_Odd(5))
---
+++ day 48.5/Homework/Cw№3.py
@@ -0,0 +1,7 @@
+about_me = {
+    "name": "Giorgi",
+    "surname": "Ghvinadze",
+    "age": 14.9
+}
+
+print(about_me.keys())
