2c66e4a4e9c719c9ff77693ac4ba542e4b9f8029
GiorgiGhvdze <ghvinadzegiorgi0@gmail.com>
2026-03-02 22:04:09 +0400

    20th commit

---
+++ day 62/Classwork/index.js
@@ -0,0 +1,75 @@
+// Cw№1
+
+// javascript გამოიყენება როგორც front end-ში ასევე  back end-ში. ის ანიჭებს საიტს ფუნქციონალობას
+
+
+// Cw№2
+
+// console.log() ის მეშვეობით ჩვენ საიტის კონსოლში გამოგვაქვს კონკრეტული კოდი
+
+
+// Cw№3
+
+// let - ით შექმნილ ცვლადს შეგვიძლია გადავუაროთ და ახალი მონაცემის გადაცემა შეიძლება
+let x = 4;
+
+x = 5
+
+console.log(x)
+
+
+// const - ით შექმნილ ცვლადს არ შეგვიძლია გადავუაროთ და ახალი მონაცემის გადაცემა შეიძლება. თუ მას შევქმნით უკვე ვეღარ შევცვლით
+
+const y = "Giorgi";
+
+// y = "Nika" - გამოიწვევს TypeError - ს
+
+console.log(y)
+
+
+// Cw№4
+
+
+// string - ბრჭყალებში მოცემული სიმბოლოები
+
+// Number - რიცხვი
+
+// boolean - true და false. პითონისგან განსხვავები ჯავასკრიპტში იწერება პატარა ასოებით
+
+// object - იგივე დიქშენარი მაგრამ key არ იწერება ბრჭყალებში
+
+// Null და Undenified - არაფერი
+
+
+
+// Cw№5
+
+console.log(1 + 1)
+console.log(2 + 4)
+console.log("a" + "b")
+
+
+console.log(1 - 1)
+console.log(1 - 99)
+console.log(11 - 15)
+
+
+console.log(1 * 1)
+console.log(19 * 0)
+console.log(1 * 15)
+
+
+console.log(1 / 1)
+console.log(19999 / 1)
+console.log(123 / 1000)
+
+
+console.log(1 ** 1)
+console.log(5 ** 14)
+console.log(111 ** 2)
+
+console.log(10 % 3)
+console.log(15 % 2)
+console.log(100 % 10)
+
+
